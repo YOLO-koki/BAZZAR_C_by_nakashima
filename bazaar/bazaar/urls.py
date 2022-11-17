a63a2558ab1978b6195769122d6ff1763ae4e8aa
FUKUNIKU <fukushi0209@gmail.com>
2022-11-17 14:31:37 +0900

    urls.py修正

--- bazaar/bazaar/urls.py
+++ bazaar/bazaar/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('top.urls')),
+    path('user/', include('user.urls')),
+    path('comp/', include('comp.urls')),
 ]
