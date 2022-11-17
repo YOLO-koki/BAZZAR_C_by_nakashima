8ab03bc6532d6743ec732405140a03563f7a0b00
FUKUNIKU <fukushi0209@gmail.com>
2022-11-17 12:38:08 +0900

    user登録修正

--- bazaar/user/urls.py
+++ bazaar/user/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path('', views.UserIndexView.as_view(),name = 'index'),#トップページ
-    path('userCheck/', UserCheckView.as_view(), name='userCheck'),
-    path('userRegist/', UserRegistView.as_view(), name='userRegist'),
-    path('userComplete/', UserCompleteView.as_view(), name='userComplete'),
-    path('userLogin/',UserLoginView.as_view(),name='userLogin'),
-    path('userMailSend/',UserMailSendView.as_view(),name='userMailSend')
+    path('userCheck/', views.UserCheckView.as_view(), name='userCheck'),
+    path('userRegist/', views.UserRegistView.as_view(), name='userRegist'),
+    path('userComplete/', views.UserCompleteView.as_view(), name='userComplete'),
+    path('userLogin/',views.UserLoginView.as_view(),name='userLogin'),
+    path('userMailSend/',views.UserMailSendView.as_view(),name='userMailSend')
     ]
--- bazaar/user/views/__init__.py
+++ bazaar/user/views/__init__.py
@@ -3,4 +3,4 @@ from .user_check_infoviews import UserCheckView
 from .user_complete_registerviews import UserCompleteView
 from .user_loginviews import UserLoginView
 from .user_mail_sendviews import UserMailSendView
-from .indexview import UserIndexView
+from .userindex_views import UserIndexView
