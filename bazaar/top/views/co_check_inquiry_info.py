359a13f602fb560ea308efe2c4e3ccb5b79bc088
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2022-11-22 12:21:51 +0900

    お問い合わせ確認

--- bazaar/top/views/co_check_inquiry_info.py
+++ bazaar/top/views/co_check_inquiry_info.py
@@ -3,6 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from django.urls import reverse_lazy
+from ..forms import InquiryForm
 
 class CoCheckInquiryInfoView(TemplateView):
     template_name = "top/co_check_inquiry_info.html"
+
+    def __init__(self):
+        self.params = {
+        'inquiry':'',
+        'Inquiry_name':'',
+        'Inquiry_email':'',
+        }
+    
+    def post(self, request):
+        Inquiry = request.POST["Inquiry"]
+        Inquiry_name = request.POST["Inquiry_name"]
+        Inquiry_email = request.POST["Inquiry_email"]
+        self.params["inquiry"] = Inquiry
+        self.params["inquiry_name"] = Inquiry_name
+        self.params["inquiry_email"] = Inquiry_email
+        df = self.params
+
+        return render(request, 'top/co_check_inquiry_info.html', df)
