7284d20997d875b81288b9745575a074d16a9a57
FUKUNIKU <fukushi0209@gmail.com>
2022-11-25 12:15:58 +0900

    db_reset

--- bazaar/accounts/admin.py
+++ bazaar/accounts/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 
 from .models import CustomUser
 
-@admin.register(CustomUser)
-class CustomUser(admin.ModelAdmin):
-    model:CustomUser
+# @admin.register()
+# class CustomUser(admin.ModelAdmin):
+#     model:CustomUser
