375f23d863d39af690c9fce3bb4bd9b203a2f3e3
FUKUNIKU <fukushi0209@gmail.com>
2022-11-25 11:39:44 +0900

    forms

--- bazaar/top/forms.py
+++ bazaar/top/forms.py
@@ -1,16 +1,11 @@
 from django import forms
 
-def clean_Inquiry_email(self):
-    Inquiry_email = self.cleaned_data['Inquiry_email']
-    if '@' not in Inquiry_email:
+def clean_Inquiry_email(value):
+    if '@' not in value:
         raise forms.ValidationError('@を含んだメールアドレスにしてください')
-    return Inquiry_email
+    return value
 
 class InquiryForm(forms.Form):
     Inquiry = forms.CharField(label = "お問い合わせ内容" , required = True ,widget = forms.Textarea)
-    Inquiry_name = forms.CharField(label = "お問い合わせお名前" , required = True , max_length = 20, widget = forms.TextInput)
-    Inquiry_email = forms.EmailField(label = "お問い合わせメールアドレス" , required = True , max_length = 50, widget = forms.TextInput,validators=[clean_Inquiry_email])
-
-
-
-
+    Inquiry_name = forms.CharField(label = "お名前" , required = True , max_length = 20, widget = forms.TextInput)
+    Inquiry_email = forms.EmailField(label = "メールアドレス" ,validators=[clean_Inquiry_email], required = True , max_length = 50, widget = forms.TextInput,)
