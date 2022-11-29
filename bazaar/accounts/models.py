27021b140b502fb29c110d3a183f9e878afb2622
FUKUNIKU <fukushi0209@gmail.com>
2022-11-29 10:39:04 +0900

    CustomUser修正

--- bazaar/accounts/models.py
+++ bazaar/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator,RegexValidator
 from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.hashers import make_password
 
 
 """
@@ -17,52 +18,57 @@ USERTYPE_COMP = 100
 USERTYPE_USER = 200
 USERTYPE_DEFAULT = USERTYPE_USER
 """
-
-
+'''
 alphanumeric = RegexValidator(
     r'^[0-9a-zA-Z]*$', 'Only alphanumeric characters are allowed.')
-
+'''
 
 class CustomUserManager(BaseUserManager):
-    """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
-    """
-    def create_user(self, mail, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
+
+    def _create_user(self, userid, mail,password,  **extra_fields):
         if not mail:
-            raise ValueError(('メールアドレスを入力してください'))
+            raise ValueError('Emailを入力して下さい')
         mail = self.normalize_email(mail)
-        user = self.model(mail=mail, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, mail, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        # extra_fields.setdefault('is_staff', True)
-        # extra_fields.setdefault('is_superuser', True)
-        # extra_fields.setdefault('is_active', True)
-
-        # if extra_fields.get('is_staff') is not True:
-        #     raise ValueError(('Superuser must have is_staff=True.'))
-        # if extra_fields.get('is_superuser') is not True:
-        #     raise ValueError(('Superuser must have is_superuser=True.'))
-
-        def get_by_natural_key(self,email, password):
-            return self.get(email = email, password=password)
+        userid = self.model.normalize_username(userid)
+        user = self.model(userid=userid, mail=mail, **extra_fields)
+        user.password = make_password(password)
         
-        return self.create_user(mail,password)
+        user.save(using=self.db)
+        return user
+    
+
+    """
+    Custom user model manager where mail is the unique identifiers
+    for authentication instead of userids.
+    """
+    def create_user(self,userid, mail, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(userid,mail, password, **extra_fields)
+
+    def create_superuser(self,userid, mail, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given mail and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(('Superuser must have is_superuser=True.'))
+
+        def get_by_natural_key(self,mail, password):
+            return self.get(mail = mail, password=password)
+        
+        return self._create_user(userid,mail, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser,PermissionsMixin):
     #ユーザーと事業者共通で必要
     #userid 主キー　１６桁
-    userid = models.CharField(verbose_name='userid',
-                          validators=[alphanumeric],primary_key=True,max_length=16,unique=True)
+    userid = models.CharField(verbose_name='userid',primary_key=True,max_length=16,unique=True)
     username = models.CharField(verbose_name='name',max_length=20)
     #Trueが一般ユーザー,Falseが事業者
     usertype = models.BooleanField(verbose_name='usertype',default=True)
@@ -73,14 +79,22 @@ class CustomUser(AbstractBaseUser,PermissionsMixin):
     age=models.IntegerField(verbose_name='age',blank=True, null=True,validators=[MaxValueValidator(150),MinValueValidator(0)])
     adress=models.CharField(verbose_name='adress',max_length=80,blank=True, null=True)
     last_login = models.DateTimeField(verbose_name='lastlogin', blank=True, null=True)
+    #アカウントの権限
+    is_superuser = models.BooleanField(("superuser status"),default=False,)
+    is_staff = models.BooleanField(("staff status"),default=False,)
+    is_active = models.BooleanField(("active status"),default=True,)
+    
+
 
     USERNAME_FIELD = 'userid'
+    EMAIL_FIELD = 'mail'
     REQUIRED_FIELDS = [
-       'mail'
+        'mail',
     ]
 
     objects = CustomUserManager()
 
     class Meta():
         verbose_name_plural = 'CustomUser'
+
 
