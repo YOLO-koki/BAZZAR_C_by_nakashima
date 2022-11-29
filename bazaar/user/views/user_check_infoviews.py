899ab7b28c9a4fd408853db9f35df42b99f38e23
FUKUNIKU <118143353+FUKUNIKU@users.noreply.github.com>
2022-11-29 11:00:38 +0900

    Revert "Test2"

--- bazaar/user/views/user_check_infoviews.py
+++ bazaar/user/views/user_check_infoviews.py
@@ -7,6 +7,26 @@ from django.urls import reverse_lazy
 class CheckRegiInfoView(TemplateView):
     template_name = "user/user_check_registed_info.html"
 
+    def __init__(self):
+        self.params = {
+        'userId':'',
+        'password':'',
+        'name':'',
+        'mail':'',
+        'phone':'',
+        }
     
+    def post(self, request):
+        user_id = request.POST["userId"]
+        password = request.POST["password"]
+        name = request.POST["name"]
+        mail = request.POST["mail"]
+        phone = request.POST["phone"]
+        self.params["user_id"] = user_id
+        self.params["password"] = password
+        self.params["name"] = name
+        self.params["mail"] = mail
+        self.params["phone"] = phone
+        df = self.params
 
-    
+        return render(request, 'user/user_check_registed_info.html', df)
