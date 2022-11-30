bbd3fe4fa03736dd49f0020dc696fd1a50acd83f
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2022-11-30 10:29:26 +0900

    保存

--- bazaar/user/forms.py
+++ bazaar/user/forms.py
@@ -1,7 +1,37 @@
 from django import forms
+import sys
+import pathlib
+currentdir = pathlib.Path(__file__).resolve().parent
+sys.path.append(str(currentdir)+"..comp/")
+# from comp.models.business_person import Business_person
+# from.models.users import User 
+from accounts.models import CustomUser
+import re
+from django.core.exceptions import ValidationError
 
-stars_list = [('1','星１'),('2','星２'),('3','星３'),('4','星４'),('5','星５')]
 
-class KutikomiForm(forms.Form):
-    stars = forms.CharField(label="評価",required=True,widget=forms.RadioSelect)
-    impressions = forms.CharField(label="感想",required=True)
+class LoginBusiness_personForm(forms.ModelForm):
+    class Meta:
+        model = CustomUser
+        fields = ('userid', 'password')
+        labels = {
+            'userid': 'ユーザーID',
+            'password': 'パスワード',
+        }
+        widgets = {
+            'userid': forms.TextInput,
+            'password': forms.TextInput,
+        }
+
+class KutikomiForm(forms.ModelForm):
+    class Meta:
+        model = 
+
+       
+
+        
+# stars_list = [('1','星１'),('2','星２'),('3','星３'),('4','星４'),('5','星５')]
+
+# class KutikomiForm(forms.Form):
+#     stars = forms.CharField(label="評価",required=True,widget=forms.RadioSelect)
+#     impressions = forms.CharField(label="感想",required=True)
