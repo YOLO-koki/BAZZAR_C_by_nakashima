d7d56dd7282f31bed00ec8c249e0772ffd521149
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2022-12-01 14:26:19 +0900

    保存

--- bazaar/top/views/co_store_detail.py
+++ bazaar/top/views/co_store_detail.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,DetailView
 from django.urls import reverse_lazy
 
-class CoStoreDetailView(TemplateView):
+class CoStoreDetailView(DetailView):
+    model = store
     template_name = "top/co_store_detail.html"
