f3e3d27872e57e1b4279bb003603e5b988501158
FUKUNIKU <fukushi0209@gmail.com>
2022-12-01 12:12:35 +0900

    passwordreset

--- bazaar/accounts/views/password_reset_views.py
+++ bazaar/accounts/views/password_reset_views.py
@@ -5,8 +5,8 @@ from ..forms import MyPasswordResetForm
 class PasswordReset(PasswordResetView):
     """パスワード変更用URLの送付ページ"""
     template_name = 'accounts/password_reset.html'
-    subject_template_name = 'accounts/email/password_reset_subject.txt'
-    email_template_name = 'accounts/email/password_reset_message.txt'
+    subject_template_name = 'accounts/email/password_reset_subject.txt' #題名
+    email_template_name = 'accounts/email/password_reset_message.txt' #内容
     
     form_class = MyPasswordResetForm
     success_url = reverse_lazy('accounts:password_reset_done')
