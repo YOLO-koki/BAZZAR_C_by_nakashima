81fb6ac6b4403f2f878434894e4efb5bd107e9f3
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2023-01-09 17:11:53 +0900

    保存

--- bazaar/top/views/top_index_views.py
+++ bazaar/top/views/top_index_views.py
@@ -10,15 +10,7 @@ class TopIndexView(ListView):
     model = Store
     template_name: str = "top/co_toppage.html"
     context_object_name = 'stores'
-
-    # def store_list(request):
-    #     template_name: str = "top/co_toppage.html"
-    #     store = {}
-    #     sl = Store.objects.all()
-    #     store["objects_list"] = sl
-
-    #     return render(request, template_name ,store)
     
-    # def search(request):
-    #     objects = Store.objects.filter(Q(store_name__iexact="1") | Q(store_name__icontains="い"))
-    #     return render(request,)
+    def search(request):
+        objects = Store.objects.filter(Q(store_name__iexact="1") | Q(store_name__icontains="い"))
+        return render(request,)
