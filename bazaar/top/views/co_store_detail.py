d5ebf2b6939947af60d9a0370fc0ec164039bb6c
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2023-01-24 12:38:51 +0900

    保存

--- bazaar/top/views/co_store_detail.py
+++ bazaar/top/views/co_store_detail.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView,DetailView
 from django.urls import reverse_lazy
 from comp.models import Store,Kuchikomi
+from django.db.models import Avg
+from django.db.models import Count
 
 
 class CoStoreDetailView(DetailView):
@@ -11,12 +13,54 @@ class CoStoreDetailView(DetailView):
     template_name = "top/co_store_detail.html"
     context_object_name = 'stores'
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self,**kwargs):
         context = super(CoStoreDetailView,self).get_context_data(**kwargs)
+        print("------------------------",kwargs['object'].pk,"------------------------")
+        pk = kwargs['object'].pk
+        avg_score = Kuchikomi.objects.filter(store_id_id = pk).aggregate(avg_score = Avg('score'))
         context.update({
-            'kuchikomi_list':Kuchikomi.objects.all()
+            'kuchikomi_list':Kuchikomi.objects.all(),
+            'avg_score':avg_score
         })
         return context
 
     def get_queryset(self):
         return Store.objects.all()
+
+    # def get_context_data(self,**kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     print("------------------------",kwargs['object'].pk,"------------------------")
+    #     context.update({
+    #         'avg_score':kwargs['object'].pk
+    #     })
+    #     return kwargs['object'].pk
+
+def avg(request,pk):
+    avg_score = Kuchikomi.objects.filter(store_id_id = pk).aaggregate(Avg('score'))
+    context = {
+        'avg_score':avg_score
+    }
+
+    return render(request,'top/co_store_detail.html',context)
+
+        
+    # def avg(request,pk):
+    #     store_count = Kuchikomi.objects.get(store_id_id=pk).count()
+        
+    #     context = {
+    #         "store_count": store_count,
+    #     }
+
+    #     return render(request,'top/co_store_detail.html',context)
+
+# ------------------------------------------------
+    # def get_context_data(self, **kwargs):
+    #     context = super(CoStoreDetailView,self).get_context_data(**kwargs)
+    #     context.update({
+    #         'kuchikomi_list':Kuchikomi.objects.all()
+    #     })
+    #     return context
+
+    # def get_queryset(self):
+    #     return Store.objects.all()
+    # ----------------------------------------------
