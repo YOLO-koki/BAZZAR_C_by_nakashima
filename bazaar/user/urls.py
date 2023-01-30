7b6e90857c07dd65bc220a25f1fe5a0d77a19323
FUKUNIKU <fukushi0209@gmail.com>
2023-01-30 14:13:51 +0900

    予約履歴

--- bazaar/user/urls.py
+++ bazaar/user/urls.py
@@ -10,6 +10,7 @@ from .views import UserKutikomiView
 from .views import UserCheckKutikomiView
 from .views import UserReviewPerfectView
 from .views import UserReservationlistView
+from .views import UserReservationView
 from .views import UserInfoView
 from .views import UserAccountUpdateView
 from .views import UserMakeReservationView
@@ -29,6 +30,7 @@ urlpatterns = [
     path('user_info/',UserInfoView.as_view(),name="user_info"),#ユーザー情報ページ
     path('user_accounts_update/<str:userid>/',UserAccountUpdateView.as_view(),name='userAccountUpdate'),#ユーザー情報更新ページ
     path('reservation_list/',UserReservationlistView.as_view(),name="reservation_list"),#予約履歴ページ
+    path('reservation/',UserReservationView.as_view(),name="reservation"),#予約確認ページ
     path('userRegist/', CreateAccountView.as_view(), name='userRegist'),#新規登録
     path('userCheck/', CheckRegiInfoView.as_view(), name='userCheck'),
     path('userMailSend/',UserMailSendView.as_view(),name='userMailSend'),
--- bazaar/user/views/__init__.py
+++ bazaar/user/views/__init__.py
@@ -14,4 +14,5 @@ from .user_info_view import UserInfoView
 from .user_accounts_update_view import UserAccountUpdateView
 from .user_make_reservation_view import UserMakeReservationView
 from .user_select_reservation_view import UserSelectReservationView
+from .user_reservation_view import UserReservationView
 from .user_make_reservation_login_view import UserMakeReservationLoginView
