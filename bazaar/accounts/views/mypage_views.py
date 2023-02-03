839a2a1b7fe16bf55b811a7eb377499eb1e9771d
Ryu7230 <knz2174487@stu.o-hara.ac.jp>
2023-02-03 13:47:16 +0900

    mypage

--- bazaar/accounts/views/mypage_views.py
+++ bazaar/accounts/views/mypage_views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from comp.models import Store,Menu
+
 
 #ログインしていることが条件
 class MypageView(LoginRequiredMixin, TemplateView):
@@ -10,3 +12,11 @@ class MypageView(LoginRequiredMixin, TemplateView):
             'usertype': self.request.user.usertype
         }
         return self.render_to_response(ctx)
+
+    def get_object(self):
+        id=Store.objects.get(bp_id_id=self.request.user)
+        store = Store.objects.filter(store_id=id).exists
+
+        # menu_id = Store.objects.get(bp_id_id=self.request.user)
+        # menu = Menu.objects.filter(store_id_id = menu_id).exists
+        return store
